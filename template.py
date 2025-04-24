6e1cfb0d72647724c1d1957cf4aed961594191f7
maichmarc <maichmarc@gmail.com>
2025-04-24 12:57:54 +0300

    Requirements and Installations added

--- template.py
+++ template.py
@@ -1,36 +1,51 @@
 import os
 from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
+project_name = "textSummarizer"
 
 list_of_files = [
-    '__init__.py',
-    'components/__init__.py',
-    'components/data_ingestion.py',
-    'components/data_transformation.py',
-    'components/model_trainer.py',
-    'notebooks/'
-    'static/css/style.css',
-    'templates/home.html',
-    'templates/index.html',
-    'pipeline/__init__.py',
-    'pipeline/training_pipeline.py',
-    'pipeline/prediction_pipeline.py',
-    'exception.py',
-    'logger.py',
-    'utils.py',
-    'app.py',
-    'requirements.txt',   
-    'setup.py'
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/utils/__init__.py",
+    "src/utils/common.py",
+    "src/logging/__init__.py",
+    "src/config/__init__.py",
+    "src/config/configuration.py",
+    "src/pipeline/__init__.py",
+    "src/entity/__init__.py",
+    "src/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "notebooks/trials.ipynb",
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir != '':
+
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath,'w') as f:
             pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        print(f'file is already present at {filepath}')
+        logging.info(f"{filename} is already exists")
