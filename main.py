ec7d5445e78079b86f1114832a645cac02b478fe
maichmarc <maichmarc@gmail.com>
2025-04-26 00:23:53 +0300

    Data Ingestion done

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.my_logging.my_logger import logging
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+# my_logger.info('Welcome to our custom logging')
+
+
---
+++ src/my_logging/my_logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), 'logs',LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# print(logging.__file__)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(module)s - %(message)s',
+    level=logging.INFO
+)
+
+
+# my_logger = logging.getLogger('TextSummariserLogger')
+
+# if __name__ == '__main__':
+#     logger.info('Logging has started')
