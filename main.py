447eb07fbe9d3feec7485f3d853d7383bb143af1
maichmarc <maichmarc@gmail.com>
2025-04-28 14:28:33 +0300

    Data Transformation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from src.my_logging.my_logger import logging
 
 
@@ -22,6 +23,18 @@ try:
 except Exception as e:
         logging.exception(e)
         raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
 
 # my_logger.info('Welcome to our custom logging')
 
