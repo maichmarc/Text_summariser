c2b364156e0c2d866bf534226638ccc67733ef82
maichmarc <maichmarc@gmail.com>
2025-04-28 12:10:19 +0300

    Data Validation done

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,6 @@
 from src.constants import *
 from src.utils.common import read_yaml, create_directories
-from src.entity import DataIngestionConfig
+from src.entity import DataIngestionConfig, DataValidationConfig
 
 
 class ConfigurationManager:
@@ -11,7 +11,7 @@ class ConfigurationManager:
     ):
 
         self.config = read_yaml(config_filepath)
-        self.params = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
 
@@ -28,3 +28,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
