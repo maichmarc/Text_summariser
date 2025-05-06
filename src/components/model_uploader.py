bda2a6ed12dc735f9950e6ab6f12fc1b0f4ac0a1
maichmarc <maichmarc@gmail.com>
2025-05-06 17:34:47 +0300

    Model uploader

--- src/components/model_uploader.py
+++ src/components/model_uploader.py
@@ -14,10 +14,10 @@ class ModelUploader:
         
     def upload_model(self):
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        # tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
-        # model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_path).to(device)
-        tokenizer = PegasusTokenizerFast.from_pretrained(self.config.tokenizer_path)
-        model_pegasus = PegasusForConditionalGeneration.from_pretrained(self.config.model_path)
+        tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path, local_files_only=True)
+        model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_path, local_files_only=True).to(device)
+        # tokenizer = PegasusTokenizerFast.from_pretrained(self.config.tokenizer_path)
+        # model_pegasus = PegasusForConditionalGeneration.from_pretrained(self.config.model_path)
 
         api = HfApi()
 
